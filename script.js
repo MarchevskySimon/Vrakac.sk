9606260edc0d212f5295371733a2fddc298aec98
MarchevskySimon <jety258@gmail.com>
2021-04-26 10:40:37 +0200

    Stranka funguje uz aj na IE (aj JS) a open_clock ma hight v PX

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 /*
  * NAV Animetion from TOP
  */
@@ -8,7 +7,7 @@
 let nav = document.getElementById('nav');
 
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 600) {
+    if (window.pageYOffset > 600) {
         nav.style.backgroundColor = 'rgb(35, 35, 35)';
         nav.style.boxShadow = '2px 2px 2px 5px rgba(15, 15, 15, .5)';
     } else {
@@ -26,7 +25,7 @@ let parallax = document.getElementById('text'),
     header = document.getElementById('header');
 
 window.addEventListener('scroll', function() {
-    var value = window.scrollY;
+    var value = window.pageYOffset;
 
     // console.log('ahoj');
     parallax.style.top = (value * 0.3) + "px";
@@ -77,8 +76,8 @@ hamburger.addEventListener('click', function() {
 var x = navUl.querySelectorAll('a'),
     i;
 
-for (i = 0; i < x.length; i++) {
-    x[i].addEventListener('click', event => {
+for (i = 0; i < x.length; i += 1) {
+    x[i].addEventListener('click', function() {
         navUl.style.left = '-270px';
         navDark.style.opacity = '0';
         navDark.style.pointerEvents = 'none';
@@ -90,12 +89,53 @@ for (i = 0; i < x.length; i++) {
 
 /* ESC event  */
 
-document.addEventListener('keyup', (e) => {
-    if (e.keyCode === 27) {
+document.addEventListener('keyup', function(ele) {
+    if (ele.keyCode === 27) {
         navUl.style.left = '-270px';
         navDark.style.opacity = '0';
         navDark.style.pointerEvents = 'none';
 
         bool = Boolean(true);
     }
+});
+
+/*
+ * Zmena karty ZBERNEHO MIESTA
+ */
+
+let sl = document.getElementById('sl'),
+    bj = document.getElementById('bj'),
+    pr = document.getElementById('pr');
+
+var cardSl = document.getElementById('stara_lubovna'),
+    cardBj = document.getElementById('bardejov'),
+    cardPr = document.getElementById('poprad');
+
+var mapIframe = document.getElementById('map_iframe');
+let srcSl = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDj4LrSDx-3UpQBSXlq8wKmRqr5hj2BxB4&center=49.30759412395534,20.6809701040204&zoom=15&q=VRAKAC.sk',
+    srcBj = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDj4LrSDx-3UpQBSXlq8wKmRqr5hj2BxB4&center=49.30021930028979,21.285474511815483&zoom=15&q=DRUHOTNÉ_SUROVINY_Lamancová_Irena',
+    srcPr = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDj4LrSDx-3UpQBSXlq8wKmRqr5hj2BxB4&center=49.0502089571958,20.2974461255149&zoom=10&q=Poprad';
+
+sl.addEventListener('click', function() {
+    cardSl.style.display = 'block';
+    mapIframe.src = srcSl;
+
+    cardBj.style.display = 'none';
+    cardPr.style.display = 'none';
+});
+
+bj.addEventListener('click', function() {
+    cardBj.style.display = 'block';
+    mapIframe.src = srcBj;
+
+    cardSl.style.display = 'none';
+    cardPr.style.display = 'none';
+});
+
+pr.addEventListener('click', function() {
+    cardPr.style.display = 'block';
+    mapIframe.src = srcPr;
+
+    cardSl.style.display = 'none';
+    cardBj.style.display = 'none';
 });
